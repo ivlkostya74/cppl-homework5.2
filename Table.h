7f7bfb9998ca80204b9eb0d43a2b7953afa09cad
Konstantin Ivlev <ivlkostya@yandex.ru>
2023-07-28 22:51:58 +1000

    first commit

---
+++ Table.h
@@ -0,0 +1,79 @@
+﻿#pragma once
+#include <iostream>
+#include <windows.h>
+
+template<class T>
+class Table
+{
+public:
+	Table(int r, int c)
+	{
+		rows = r;
+		cols = c;
+		elements = new T * [rows];
+		for (int i = 0; i < rows; ++i)
+			elements[i] = new T[cols]();
+	}
+
+	~Table()
+	{
+		for (int i = 0; i < rows; ++i)
+			delete[] elements[i];
+		delete[] elements;
+		elements = nullptr;
+	}
+
+	const T* operator[](const int row) const
+	{
+		if ((row < 0) || (row >= rows)) throw std::runtime_error("Некорректный индекс обращения к таблице по индексу");
+		return elements[row];
+	}
+
+	T* operator[](const int row)
+	{
+		if ((row < 0) || (row >= rows)) throw std::runtime_error("Некорректный индекс обращения к таблице в операторе присвоения по индексу");
+		return elements[row];
+	}
+
+	Table& operator=(Table t2)
+	{
+		if (this != &t2) 
+		{
+			for (int i = 0; i < rows; ++i)
+				delete[] elements[i];
+			delete[] elements;
+			elements = new T * [t2.rows];
+			for (int i = 0; i < t2.rows; ++i)
+				elements[i] = new T[t2.cols]();
+			rows = t2.rows;
+			cols = t2.cols;
+			for (int i = 0; i < rows; ++i)
+				for (int j = 0; j < cols; ++j)
+					elements[i][j] = t2[i][j];
+		}
+
+		return *this;
+	}
+
+	Table(Table& t)
+	{
+		elements = new T * [t.rows];
+		for (int i = 0; i < t.rows; ++i)
+			elements[i] = new T[t.cols]();
+		rows = t.rows;
+		cols = t.cols;
+		for (int i = 0; i < rows; ++i)
+			for (int j = 0; j < cols; ++j)
+				elements[i][j] = t[i][j];
+	}
+
+	int Size() const
+	{
+		return rows * cols;
+	}
+
+private:
+	T** elements = nullptr;
+	int rows = 0;
+	int cols = 0;
+};
---
+++ cppl-homework5.2.cpp
@@ -0,0 +1,100 @@
+﻿#include "Table.h"
+#include <iostream>
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	int row = 5;
+	int col = 3;
+
+	Table<int> newTable(row, col);
+
+	newTable[0][0] = 4;
+	newTable[1][2] = 5;
+
+	std::cout << newTable[0][0] << std::endl;
+	std::cout << newTable[1][2] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << newTable.Size() << std::endl;
+	std::cout << std::endl;
+
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+			std::cout << newTable[i][j] << "\t";
+		std::cout << std::endl;
+	}
+
+	Table<int> nt(newTable);
+
+	std::cout << std::endl;
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+			std::cout << nt[i][j] << "\t";
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	row = 4;
+	col = 2;
+
+	Table<int> newTable2(row, col);
+
+	newTable2[0][1] = 3;
+	newTable2[1][1] = 2;
+	newTable2[3][0] = 10;
+
+	try
+	{
+		std::cout << "newTable2[8][1] = 4;" << std::endl;
+		newTable2[8][1] = 4;
+	}
+	catch (std::runtime_error& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+
+	std::cout << std::endl;
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+			std::cout << newTable2[i][j] << "\t";
+		std::cout << std::endl;
+	}
+
+	nt = newTable2;
+
+	std::cout << std::endl;
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < col; ++j)
+			std::cout << nt[i][j] << "\t";
+		std::cout << std::endl;
+	}
+
+	std::cout << std::endl;
+
+	const Table<int> newTable3(nt);
+
+	std::cout << newTable3[1][1] << std::endl;
+	std::cout << newTable3[3][0] << std::endl;
+
+	std::cout << std::endl;
+
+	try
+	{
+		std::cout << "std::cout << newTable3[10][10] << std::endl;" << std::endl;
+		std::cout << newTable3[10][10] << std::endl;
+	}
+	catch (std::runtime_error& ex)
+	{
+		std::cout << ex.what() << std::endl;
+	}
+
+	return 0;
+}
